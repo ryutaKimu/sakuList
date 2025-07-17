15136fa414436c5198ebe402e62ce201c67412d2
ryutaKimu <rekkoilove@softbank.ne.jp>
2025-07-17 12:42:45 +0900

    差分解消

--- frontend/src/admin/api/admin.ts
+++ frontend/src/admin/api/admin.ts
@@ -9,8 +9,6 @@ export const postMember = async (formData: FormData) => {
   });
   return response.data;
 };
-<<<<<<< Updated upstream
-=======
 
 export const fetchMember = async(id?:string)=>{
   const response = await adminApi.get(`admin/member/detail/${id}`)
@@ -21,4 +19,3 @@ export const updateMember = async(id:string, payload: UpdateMember)=>{
   const response = await adminApi.put(`admin/dashboard/member/${id}`, payload)
   return response.data
 }
->>>>>>> Stashed changes
